6e6f01e5e411d7220d95853aa4073078c1ee955e
Everto Jose Farias Duran <efariasd@falabella.cl>
2024-05-20 13:19:44 -0400

    Update consultas.js

--- consultas.js
+++ consultas.js
@@ -2,21 +2,19 @@ const fs = require("fs")
 const { v4: uuidv4 } = require('uuid');
 require('dotenv').config();
 const path = require('path');
+const database = require("./usuarios")
 
 // Ruta al archivo que quieres leer
-const filePath = path.join(__dirname, 'usuarios.js');
-
 exports.nuevoUsuario = (email, name, password) => {
-    try {
-      const result = fs.readFileSync(filePath, { encoding: 'utf8' });
-      const {usuarios} = eval(result);
-      let exist_usuario = usuarios.find(usuario => usuario.email)
+  try {
+      const {usuarios} = eval(database);
+      let exist_usuario = usuarios.find(usuario => usuario.email === email)
       if(exist_usuario){
         throw new Error("Usuario ya existe")
       }
       usuarios.push({ id: uuidv4(), name, email, password });
       const newFileContent = `module.exports = ${JSON.stringify({ usuarios }, null, 2)};`;
-      fs.writeFileSync(filePath, newFileContent, { encoding: 'utf8' });
+      fs.writeFileSync("usuarios.js", newFileContent, { encoding: 'utf8' });
     } catch (error) {
       console.error("Error al crear al usuario:", error);
       throw new Error(error.message)
@@ -25,8 +23,7 @@ exports.nuevoUsuario = (email, name, password) => {
 
 exports.getUsuario = async (email, password) => {
   try {
-    const result = fs.readFileSync(filePath, { encoding: 'utf8' });
-    const {usuarios} = eval(result);
+    const {usuarios} = eval(database);
     let usuario = usuarios.find(usuario => usuario.email === email && usuario.password === password)
     return usuario;
   } catch (e) {
--- usuarios.js
+++ usuarios.js
@@ -1,1 +1,16 @@
-module.exports = { usuarios: [] };
+module.exports = {
+  "usuarios": [
+    {
+      "id": "c8d77fc9-3b03-477e-95de-9b3f15255dca",
+      "name": "everto",
+      "email": "everto@gmail.com",
+      "password": "123456"
+    },
+    {
+      "id": "f552c7d5-e8a7-452f-9438-be1369f368fc",
+      "name": "everto",
+      "email": "everto2@gmail.com",
+      "password": "123456"
+    }
+  ]
+};
