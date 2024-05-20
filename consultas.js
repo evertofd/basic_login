0128fbd1e9fd480243ed1c23980a385224f2e291
Everto Jose Farias Duran <efariasd@falabella.cl>
2024-05-20 13:11:37 -0400

    Update name usuarios.json

--- consultas.js
+++ consultas.js
@@ -1,18 +1,22 @@
 const fs = require("fs")
 const { v4: uuidv4 } = require('uuid');
-require('dotenv').config(); 
+require('dotenv').config();
+const path = require('path');
+
+// Ruta al archivo que quieres leer
+const filePath = path.join(__dirname, 'usuarios.js');
 
 exports.nuevoUsuario = (email, name, password) => {
     try {
-      const result = fs.readFileSync("usuarios.json", { encoding: 'utf8', flag: 'r' });
-      const resp = JSON.parse(result);
-      const { usuarios } = resp;
+      const result = fs.readFileSync(filePath, { encoding: 'utf8' });
+      const {usuarios} = eval(result);
       let exist_usuario = usuarios.find(usuario => usuario.email)
       if(exist_usuario){
         throw new Error("Usuario ya existe")
       }
       usuarios.push({ id: uuidv4(), name, email, password });
-      fs.writeFileSync("usuarios.json", JSON.stringify({ usuarios }));
+      const newFileContent = `module.exports = ${JSON.stringify({ usuarios }, null, 2)};`;
+      fs.writeFileSync(filePath, newFileContent, { encoding: 'utf8' });
     } catch (error) {
       console.error("Error al crear al usuario:", error);
       throw new Error(error.message)
@@ -21,9 +25,8 @@ exports.nuevoUsuario = (email, name, password) => {
 
 exports.getUsuario = async (email, password) => {
   try {
-    const result = fs.readFileSync("usuarios.json", { encoding: 'utf8', flag: 'r' });
-    const resp = JSON.parse(result);
-    const { usuarios } = resp;
+    const result = fs.readFileSync(filePath, { encoding: 'utf8' });
+    const {usuarios} = eval(result);
     let usuario = usuarios.find(usuario => usuario.email === email && usuario.password === password)
     return usuario;
   } catch (e) {
